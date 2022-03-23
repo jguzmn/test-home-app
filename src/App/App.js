152eda2e3b2b14beb04e1944013877e841e04331
Hemel Parody <hemel.parody@wizeline.com>
2022-03-23 12:49:38 -0500

    Feature: adding global styles and styled-components library

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "Router";
 
 import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
+import Header from "../components/Header";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <BrowserRouter>
       <ErrorBoundary>
+        <GlobalStyle />
+        <Header />
         <Router />
       </ErrorBoundary>
     </BrowserRouter>
---
+++ src/App/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  body {
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Nunito', sans-serif;
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
