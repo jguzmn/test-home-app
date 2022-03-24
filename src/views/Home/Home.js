2bf4145ebbc739338db44e033e2abaeed1d60895
Hemel Parody <hemel.parody@wizeline.com>
2022-03-24 02:10:37 -0500

    Feature: adding Carousel to Home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,16 @@
+import { Fragment, useState } from "react";
+import mockedBanners from "mocks/en-us/featured-banners.json";
+
+import Carousel from "components/UI/Carousel";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [bannerImages, setBannerImages] = useState(mockedBanners.results);
+  return (
+    <Fragment>
+      <Carousel items={bannerImages} showTitle></Carousel>
+    </Fragment>
+  );
 };
 
 export default Home;
