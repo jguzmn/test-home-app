c3d19e34a50eae564d8d77d5a5036bc2345f4832
Hemel Parody <hemel.parody@wizeline.com>
2022-03-30 12:54:59 -0500

    Feature: add responsive design to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,14 @@ import styled from "styled-components";
 
 import { MAIN_COLOR } from "constants/styles";
 
+import {
+  XS_BREAK_POINT_PX,
+  SM_BREAK_POINT_PX,
+  MD_BREAK_POINT_PX,
+  LG_BREAK_POINT_PX,
+  XL_BREAK_POINT_PX,
+} from "constants/styles";
+
 const FooterContainer = styled.div`
   left: 0;
   bottom: 0;
@@ -11,6 +19,36 @@ const FooterContainer = styled.div`
   width: fill-available;
   background-color: ${MAIN_COLOR};
   padding: 2rem 8rem;
+
+  @media only screen and (max-width: ${XS_BREAK_POINT_PX}) {
+    & {
+      padding: 1rem 2rem;
+    }
+  }
+
+  @media only screen and (min-width: ${SM_BREAK_POINT_PX}) {
+    & {
+      padding: 1rem 2rem;
+    }
+  }
+
+  @media only screen and (min-width: ${MD_BREAK_POINT_PX}) {
+    & {
+      padding: 1rem 4rem;
+    }
+  }
+
+  @media only screen and (min-width: ${LG_BREAK_POINT_PX}) {
+    & {
+      padding: 1rem 6rem;
+    }
+  }
+
+  @media only screen and (min-width: ${XL_BREAK_POINT_PX}) {
+    & {
+      padding: 1rem 8rem;
+    }
+  }
 `;
 
 const FooterText = styled.span`
